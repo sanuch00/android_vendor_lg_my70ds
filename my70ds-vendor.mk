59042b0f7f76c54a5e9be37bfa20e02aed1e341e
sanuch00 <san_sanych00@mail.ru>
2017-11-25 20:23:56 +0300

    initial

---
+++ my70ds-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lg/my70ds/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lg/my70ds/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lg/my70ds/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lg/my70ds/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lg/my70ds/proprietary/vendor,system/vendor)
